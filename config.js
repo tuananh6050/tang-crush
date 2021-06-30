e201c3331737c9e8bb70901abf9b1f22e53af1c3
tuananh6050 <86720819+tuananh6050@users.noreply.github.com>
2021-06-30 23:49:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng anh đã yêu Mai ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là yêu anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không, lêu lêu :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Cuối tuần mình đi chơi em nhé!',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Cuối tuần mình đi chơi emiu của anh nhé!',
+    btnAccept: 'Vâng ạa <3',
     messLink: 'http://fb.com/messages/t/100017348769094'
 }
